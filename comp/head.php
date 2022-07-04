4f7a6bba1697e3e93750041df83138ac1f7cab48
Sajith Udurawana <sajith@Sajiths-MacBook-Air.local>
2022-07-04 15:23:25 +0530

    Google analytics and cookie popup

--- comp/head.php
+++ comp/head.php
@@ -5,7 +5,7 @@
 <meta property="og:title" content="Gulf MCQ Pro - The best free Prometricᵀᴹ Qbank for GPs">
 <meta property="og:description" content="The best free Prometricᵀᴹ Qbank for for DHA, MOH, HAAD, DHCC, OMSB, SLE, QCHP, NHRA, PLAB and USMLE exams | Become a GP in UAE, Dubai, Saudi Arabia, Abu Dhabi, Bahrain, Oman and Qatar.">
 <meta property="og:type" content="website">
-<meta property="og:image" content="/image/og-image.webp">
+<meta property="og:image" content="/static/img/og-image.webp">
 <meta property="og:url" content="https://gulfmcq.pro">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="">
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
@@ -14,3 +14,15 @@
 <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&amp;family=Bitter&amp;display=swap" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.8.0/dist/cookieconsent.css">
 <link rel="stylesheet" href="/static/css/main.css">
+<title>Gulf MCQ Pro - The best free Prometricᵀᴹ based DHA, MOH, HAAD, DHCC, OMSB, SLE, QCHP, NHRA, PLAB and USMLE Qbank | Become a GP in UAE, Dubai, Saudi Arabia, Abu Dhabi, Bahrain, Oman and Qatar.</title>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-JBY7WMM40M"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-JBY7WMM40M');
+</script>
